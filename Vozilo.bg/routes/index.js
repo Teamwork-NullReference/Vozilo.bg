2965c985ad7485e433c565ea6877e2425952c4fe
Stoiko Neykov <stoikoneykov@gmail.com>
2016-11-23 13:21:55 +0200

    adding routes/index.js

---
+++ Vozilo.bg/routes/index.js
@@ -0,0 +1,11 @@
+/* globals require __dirname*/
+'use strict';
+
+let fs = require('fs'),
+    path = require('path');
+
+module.exports = function(app, upload) {
+    fs.readdirSync(__dirname)
+        .filter(file => file.indexOf('-router') >= 0)
+        .forEach(file => require(path.join(__dirname, file))(app));
+};
