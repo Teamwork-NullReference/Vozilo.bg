0c7daae602dc7fcff5edbd6420b77a8cf704d319
madmarks <bratmiro@gmail.com>
2016-11-28 03:03:37 +0200

    connection strings with passwords removed (insert your password)

---
+++ Vozilo.bg/cars-scraper/config/connection-strings.js
@@ -0,0 +1,7 @@
+'use strict';
+
+module.exports = {
+    // uri: 'mongodb://localhost/voziloBG',
+    // uri: 'mongodb://<your-username>:<your-super-secret-password>@127.0.0.1:27019/appharbor_t72wlp1s',
+    uri: 'mongodb://<your-username>:<your-super-secret-password>@ds163677.mlab.com:63677/appharbor_t72wlp1s'
+};
