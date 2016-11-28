ba976ac7f8ca6063c5eac4df2b04e5d011976151
madmarks <bratmiro@gmail.com>
2016-11-28 02:40:57 +0200

    Revert "connection string with password removed (insert your password)"
    This reverts commit 412a27652fd5c4416be104c8d4c344286ade3ece.

--- Vozilo.bg/cars-scraper/config/constants.js
+++
@@ -1,9 +0,0 @@
-/* globals require */
-'use strict';
-
-module.exports = {
-    // connectionString: 'mongodb://localhost/voziloBG',
-    // connectionString: 'mongodb://miro:<insert-your-secret-password-here>@127.0.0.1:27019/appharbor_t72wlp1s',
-    connectionString: 'mongodb://miro:<insert-your-secret-password-here>@ds163677.mlab.com:63677/appharbor_t72wlp1s',
-    carBrandsUrl: 'http://www.cars-data.com/en/car-brands-cars-logos.html'
-};
