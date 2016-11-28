d1f78b37ebdb4dbbf9ba63e7e4a041bfab70dbb0
GalinStoychev <stoychev.galin@gmail.com>
2016-11-28 16:41:32 +0200

    car-brand-model.js created and createForm.pug updated

--- Vozilo.bg/config/passport.js
+++ Vozilo.bg/config/passport.js
@@ -29,6 +29,7 @@ module.exports= function (app, data) {
         data.findUserById(id)
             .then(user => {
                 if (user) {
+                    console.log(user);
                     return done(null, user);
                 }
                 return done(null, false);
--- Vozilo.bg/controllers/car-controller.js
+++ Vozilo.bg/controllers/car-controller.js
@@ -1,43 +1,24 @@
 /* globals module */
 'use strict';
 
-function getFakePredefinedCarsData() {
-    return [{
-        brand: 'Ford',
-        models: ['Focus', 'Ka', 'Mustang']
-    }, {
-        brand: 'Peugeot',
-        models: ['307', '405', '607']
-    }, {
-        brand: 'Opel',
-        models: ['Vectra', 'Astra', 'Kadet', 'Corsa']
-    }, {
-        brand: 'Renaut',
-        models: ['19', 'Megan']
-    }, {
-        brand: 'BMW',
-        models: ['3 series', '5 series', 'X5', 'X6']
-    }];
-}
-
 module.exports = function (data) {
     return {
         loadCreateCarForm(req, res) {
-            // data.getPredifinedCars()
-
-            let predefinedCars = getFakePredefinedCarsData();
-
-            return res.render('car/createForm', {
-                result: {
-                    user: req.user,
-                    predefinedCars
-                }
-            });
+            data.getAllBrands()
+                .then(brands => {
+                    let predefinedCars = brands;
+                    return res.render('car/createForm', {
+                        result: {
+                            user: req.user,
+                            predefinedCars
+                        }
+                    });
+                });
         },
         craeteCar(req, res) {
             let user = req.user;
-            res.send('it works all the time');
-            console.log(req.body);
+            res.send(user);
+            console.log(req.user);
             //  data.CreateNewCar()
         }
 
