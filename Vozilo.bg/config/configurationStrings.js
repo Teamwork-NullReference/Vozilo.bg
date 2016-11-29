7ac4a939f1533ce4eef09ce8b1563fe23f8c06a1
Yankov <stoian_y@yahoo.com>
2016-11-29 16:04:59 +0200

    config/configurationStrings.js removed

--- Vozilo.bg/config/configurationStrings.js
+++
@@ -1,8 +0,0 @@
-module.exports ={
-    googleCredentials: {
-        clientID: '835400022458-cle829hrklv5k8n7rnft5kgqa3krs6t6.apps.googleusercontent.com',
-        clientSecret: 'zUKbDgX29roV7DSXDdiiA1th',
-        callbackURL: 'http://127.0.0.1:3001/auth/google/callback',
-        profile: ['https://www.googleapis.com/auth/plus.login', 'https://www.googleapis.com/auth/plus.profile.emails.read']
-    }
-};
