a7a8ef8b006944e6aaf42b468a2e4e2cad3ece46
madmarks <bratmiro@gmail.com>
2016-12-04 18:22:57 +0200

    send message implemented

--- controllers/messages-controller.js
+++ controllers/messages-controller.js
@@ -122,15 +122,72 @@ module.exports = function ({ data }) {
                     res.status(200).render('correspondence/detailed-view', {
                         result: {
                             user: req.user,
+                            id: correspondenceDetails._id,
                             car: correspondenceDetails.car,
                             carOwner: correspondenceDetails.carOwner,
                             renter: correspondenceDetails.renter,
-                            messages: correspondenceDetails.messages
+                            messages: correspondenceDetails.messages.reverse()
                         }
                     });
+
+                    return Promise.resolve();
                 })
                 .catch((error) => {
                     console.log(error);
+                });
+        },
+        sendMessage(req, res) {
+            let username = req.params.username;
+            const rentalId = req.params.correspondenceId;
+            data.getUserByUsername(username)
+                .then(user => {
+
+                    if (user === null || typeof user === 'undefined') {
+                        return res.render('profile/user-not-found', {
+                            result: {
+                                user: req.user
+                            }
+                        });
+                    }
+
+                    let isAuthorizedUser = false;
+
+                    if (req.user &&
+                        req.user.role &&
+                        (req.user.role.indexOf('admin') >= 0 || username === req.user.username)) {
+                        isAuthorizedUser = true;
+                    }
+
+                    if (isAuthorizedUser) {
+                        const message = {
+                            text: req.body.inputData,
+                            date: Date.now(),
+                            sender: username
+                        };
+
+                        return data.addMessageToRental(rentalId, message);
+                    }
+
+                    // user is unauthorized
+                    res.status(401).render('unauthorized', {
+                        result: {
+                            user: req.user
+                        }
+                    });
+
+                    return Promise.reject(`User: ${username} is unauthorized.`);
+                })
+                .then(() => {
+                    return res.status(300).redirect(`${rentalId}`);
+                })
+                .catch((error) => {
+                    console.log(error);
+
+                    if (req.body.inputData || req.body.inputData.length === 0) {
+                        // stop post request and then redirect to get route
+                        res.end();
+                        res.status(300).redirect(`${rentalId}`);
+                    }
                 });
         }
     };
--- data/validation/rental-validator.js
+++ data/validation/rental-validator.js
@@ -1,8 +1,8 @@
 /* globals module */
 'use strict';
 
-const MESSAGE_LENGTH = 'Съобщението трябва да бъде между 10 и 2000 символа',
-    MESSAGE_MIN_LENGTH = 10,
+const MESSAGE_LENGTH = 'Съобщението трябва да бъде между 2 и 2000 символа',
+    MESSAGE_MIN_LENGTH = 2,
     MESSAGE_MAX_LENGTH = 2000;
 
 module.exports = function(validator) {
@@ -12,10 +12,10 @@ module.exports = function(validator) {
         },
         validateMessage(message) {
             return new Promise((resolve, reject) => {
-                let escapedMessage = validator.escapeProhibitedChars(message);
+                message.text = validator.escapeProhibitedChars(message.text);
 
-                if (!validator.validateLength(escapedMessage, MESSAGE_MIN_LENGTH, MESSAGE_MAX_LENGTH)) {
-                    reject(MESSAGE_LENGTH);
+                if (!validator.validateLength(message.text, MESSAGE_MIN_LENGTH, MESSAGE_MAX_LENGTH)) {
+                    return reject(MESSAGE_LENGTH);
                 }
 
                 resolve(message);
