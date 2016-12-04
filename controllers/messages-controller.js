6b793b485abc99a047fb66e81bc2c8b36422404f
madmarks <bratmiro@gmail.com>
2016-12-04 19:59:40 +0200

    WIP (work in progress): view latest messages

--- controllers/messages-controller.js
+++ controllers/messages-controller.js
@@ -25,46 +25,7 @@ module.exports = function ({ data }) {
                     }
 
                     if (isAuthorizedUser) {
-                        // TODO: get last messages of any correspondence, sorted by date
-                        // return data.getFilteredCars({ city, startDate, endDate, page, pageSize })
-
-                        return Promise.resolve({
-                            result: {
-                                user: req.user,
-                                correspondences: [{
-                                    id: '5843ef4e57b02e238cf56f32',
-                                    sender: {
-                                        firstName: 'Dwayne',
-                                        lastName: 'Johnson',
-                                        pictureUrl: 'https://images-na.ssl-images-amazon.com/images/M/MV5BMTkyNDQ3NzAxM15BMl5BanBnXkFtZTgwODIwMTQ0NTE@._V1_UX214_CR0,0,214,317_AL_.jpg',
-                                        address: {
-                                            city: 'София',
-                                            street: 'Цариградско шосе 123'
-                                        }
-                                    },
-                                    message: {
-                                        text: 'Are you talking to me?',
-                                        datetime: '30.12.2016 14:00'
-                                    }
-                                },
-                                {
-                                    id: '5843ef4e57b02e238cf56f32',
-                                    sender: {
-                                        firstName: 'Mad',
-                                        lastName: 'Marks',
-                                        pictureUrl: 'http://i.imgur.com/y7Hm9.jpg',
-                                        address: {
-                                            city: 'София',
-                                            street: 'Цариградско шосе 100'
-                                        }
-                                    },
-                                    message: {
-                                        text: 'Ще наема колата за един месец. Каква ще е отстъпката?',
-                                        datetime: '3.12.2016 15:00'
-                                    }
-                                }]
-                            }
-                        });
+                        return data.getLastMessagesByUsername(username);
                     }
 
                     // user is unauthorized
@@ -77,7 +38,14 @@ module.exports = function ({ data }) {
                     return Promise.reject(`User: ${username} is unauthorized.`);
                 })
                 .then((latestCorrespondences) => {
-                    res.status(200).render('correspondence/latests', latestCorrespondences);
+                    res.status(200).render('correspondence/latests', {
+                        result: {
+                            user: req.user,
+                            correspondences: latestCorrespondences
+                        }
+                    });
+
+                    return Promise.resolve();
                 })
                 .catch((error) => {
                     console.log(error);
--- data/rental-data.js
+++ data/rental-data.js
@@ -27,6 +27,26 @@ module.exports = function ({
                 });
             });
         },
+        getLastMessagesByUsername(username) {
+            return new Promise((resolve, reject) => {
+                Rental.find({
+                    $or: [{
+                        'carOwner.username': username
+                    }, {
+                        'renter.username': username
+                    }]
+
+                })
+                .select('car carOwner renter messages')
+                .exec((err, result) => {
+                    if (err) {
+                        return reject(err);
+                    }
+
+                    return resolve(result);
+                });
+            });
+        },
         addRental(rentalInfo) {
             return rentalValidator.validateMessage(rentalInfo.messageText)
                 .then(() => {
@@ -49,7 +69,7 @@ module.exports = function ({
                         renterImageUrl,
                         messageSender
                     } = rentalInfo,
-                    carProjection = {
+                        carProjection = {
                             id: car._id,
                             brand: car.brand,
                             model: car.model
@@ -98,20 +118,20 @@ module.exports = function ({
         getRentalsByUsername(username) {
             return new Promise((resolve, reject) => {
                 Rental.find({
-                        $or: [{
-                            'carOwner.username': username
-                        }, {
-                            'renter.username': username
-                        }]
+                    $or: [{
+                        'carOwner.username': username
+                    }, {
+                        'renter.username': username
+                    }]
 
-                    })
-                    .select('car rentalInfo carOwner renter')
-                    .exec((err, result) => {
-                        if (err) {
-                            return reject(err);
-                        }
-                        return resolve(result);
-                    });
+                })
+                .select('car rentalInfo carOwner renter')
+                .exec((err, result) => {
+                    if (err) {
+                        return reject(err);
+                    }
+                    return resolve(result);
+                });
             });
         },
         changeRentalStatus(newStatus, carId, rentalId) {
