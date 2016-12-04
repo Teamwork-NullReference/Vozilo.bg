Teamwork-NullReference/Vozilo.bg

commit a7a8ef8b006944e6aaf42b468a2e4e2cad3ece46

send message implemented

data/validation/rental-validator.js
@@ -1,8 +1,8 @@
 /* globals module */
 'use strict';
 
-const MESSAGE_LENGTH = 'Съобщението трябва да бъде между 10 и 2000 символа',
-    MESSAGE_MIN_LENGTH = 10,
+const MESSAGE_LENGTH = 'Съобщението трябва да бъде между 2 и 2000 символа',
+    MESSAGE_MIN_LENGTH = 2,
     MESSAGE_MAX_LENGTH = 2000;
 
 module.exports = function(validator) {
@@ -12,10 +12,10 @@ module.exports = function(validator) {
         },
         validateMessage(message) {
             return new Promise((resolve, reject) => {
-                let escapedMessage = validator.escapeProhibitedChars(message);
+                message.text = validator.escapeProhibitedChars(message.text);
 
-                if (!validator.validateLength(escapedMessage, MESSAGE_MIN_LENGTH, MESSAGE_MAX_LENGTH)) {
-                    reject(MESSAGE_LENGTH);
+                if (!validator.validateLength(message.text, MESSAGE_MIN_LENGTH, MESSAGE_MAX_LENGTH)) {
+                    return reject(MESSAGE_LENGTH);
                 }
 
                 resolve(message);

controllers/messages-controller.js
@@ -122,15 +122,72 @@ module.exports = function ({ data }) {
                     res.status(200).render('correspondence/detailed-view', {
                         result: {
                             user: req.user,
+                            id: correspondenceDetails._id,
                             car: correspondenceDetails.car,
                             carOwner: correspondenceDetails.carOwner,
                             renter: correspondenceDetails.renter,
-                            messages: correspondenceDetails.messages
+                            messages: correspondenceDetails.messages.reverse()
                         }
                     });
+
+                    return Promise.resolve();
                 })
                 .catch((error) => {
                     console.log(error);
+                });
+        },
+        sendMessage(req, res) {
+            let username = req.params.username;
+            const rentalId = req.params.correspondenceId;
+            data.getUserByUsername(username)
+                .then(user => {
+
+                    if (user === null || typeof user === 'undefined') {
+                        return res.render('profile/user-not-found', {
+                            result: {
+                                user: req.user
+                            }
+                        });
+                    }
+
+                    let isAuthorizedUser = false;
+
+                    if (req.user &&
+                        req.user.role &&
+                        (req.user.role.indexOf('admin') >= 0 || username === req.user.username)) {
+                        isAuthorizedUser = true;
+                    }
+
+                    if (isAuthorizedUser) {
+                        const message = {
+                            text: req.body.inputData,
+                            date: Date.now(),
+                            sender: username
+                        };
+
+                        return data.addMessageToRental(rentalId, message);
+                    }
+
+                    // user is unauthorized
+                    res.status(401).render('unauthorized', {
+                        result: {
+                            user: req.user
+                        }
+                    });
+
+                    return Promise.reject(`User: ${username} is unauthorized.`);
+                })
+                .then(() => {
+                    return res.status(300).redirect(`${rentalId}`);
+                })
+                .catch((error) => {
+                    console.log(error);
+
+                    if (req.body.inputData || req.body.inputData.length === 0) {
+                        // stop post request and then redirect to get route
+                        res.end();
+                        res.status(300).redirect(`${rentalId}`);
+                    }
                 });
         }
     };

routes/messages-router.js
@@ -10,6 +10,8 @@ module.exports = function({ app, controllers }) {
 
     router.get('/:username/messages/:correspondenceId', controller.getCorrespondenceDetails);
 
+    router.post('/:username/messages/:correspondenceId', controller.sendMessage);
+
     router.get('/:username/messages', controller.getLatestMessages);
 
     app.use('/user', router);

views/correspondence/detailed-view.pug
@@ -17,16 +17,17 @@ block body-content
             div.col-md-9
                 div.row
                     div.col-md-12
-                        div.row
-                            div.col-md-1
-                                div.circle-avatar
-                                    img.img-circle(src=`${result.user.picture}`, "User image")
-                            div.col-md-10
-                                textarea(class="form-control" id="inputBody" rows="6")
-                        div.row
-                            div.col-md-1
-                            div.col-md-2
-                                button(class="btn btn-success btn-sm" title="Send Message") Send Message
+                        form(method="post" action=`${result.id}`)
+                            div.row
+                                div.col-md-1
+                                    div.circle-avatar
+                                        img.img-circle(src=`${result.user.picture}`, "User image")
+                                div.col-md-10
+                                    textarea(class="form-control" rows="6" name="inputData" cols="12")
+                            div.row
+                                div.col-md-1
+                                div.col-md-2
+                                    button(class="btn btn-success btn-sm" title="Send Message") Send Message
                         div.row
                             br
                         div.row

commit f9c94f599b41fb4141152ddab0dd081b8a960fac

fix bug introduced in commit a7a8ef8b006944e6aaf42b468a2e4e2cad3ece46

data/rental-data.js
@@ -105,7 +105,7 @@ module.exports = function ({
                 });
         },
         addMessageToRental(rentalId, message) {
-            return rentalValidator.validateMessage(message)
+            return rentalValidator.validateMessage(message.text)
                 .then(() => {
                     return this.getRentalById(rentalId);
                 })

data/validation/rental-validator.js
@@ -9,9 +9,9 @@ module.exports = function (validator) {
     return {
         validateMessage(message) {
             return new Promise((resolve, reject) => {
-                message.text = validator.escapeProhibitedChars(message.text);
+                let escapedMessage = validator.escapeProhibitedChars(message);
 
-                if (!validator.validateLength(message.text, MESSAGE_MIN_LENGTH, MESSAGE_MAX_LENGTH)) {
+                if (!validator.validateLength(escapedMessage, MESSAGE_MIN_LENGTH, MESSAGE_MAX_LENGTH)) {
                     return reject(MESSAGE_LENGTH);
                 }
 